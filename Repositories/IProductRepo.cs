7cb6450fe00a93917bbf061cde61dbbf7bc159ba
Alanoud <Alanoud2022it@gmail.com>
2024-12-12 14:31:20 +0400

    creat IProductRepo

---
+++ Repositories/IProductRepo.cs
@@ -0,0 +1,14 @@
+﻿using EndPointParametersTask.Models;
+
+namespace EndPointParametersTask.Repositories
+{
+    public interface IProductRepo
+    {
+        IEnumerable<Product> GetAllProducts(int pageNumber, int pageSize);
+        Product GetProductById(int Productid);
+        Product AddProduct(Product product);
+        Product UpdateProduct(Product product);
+        bool DeleteProduct(Product product);
+
+    }
+}
