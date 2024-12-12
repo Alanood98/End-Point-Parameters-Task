f1ff73bc9bb4b6e1e9b5666e05f900e1b542eec1
Alanoud <Alanoud2022it@gmail.com>
2024-12-12 12:38:29 +0400

    Creat DataBase

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using EndPointParametersTask.Models;
+
+namespace EndPointParametersTask
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EndPointParametersTask.Models
+{
+    public class Product
+    {
+        [Key] // Marks this property as the primary key
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Auto-generates the value
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public DateTime? DateAdded { get; set; }
+    }
+}
