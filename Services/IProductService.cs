4a805a462869e5750ca94854216c7581c09cef01
Alanoud <Alanoud2022it@gmail.com>
2024-12-12 14:54:44 +0400

    creat IProductService

---
+++ Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using EndPointParametersTask.ProductDTO;
+using System.Collections.Generic;
+namespace EndPointParametersTask.Services
+{
+    public interface IProductService
+    {
+        List<ProductOutputDTO> GetAllProducts(int pageNumber, int pageSize);
+        ProductOutputDTO GetProductById(int id);
+        ProductOutputDTO AddProduct(ProductInputDTO productInput);
+        ProductOutputDTO UpdateProduct(int id, ProductInputDTO productInput);
+        string DeleteProduct(int id);
+    }
+}
