a3f2c1e141f14515412cc29ee4fe5b97c779d537
Alanoud <Alanoud2022it@gmail.com>
2024-12-12 13:25:49 +0400

    creat ProductDTO

---
+++ ProductDTO/ProductInputDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace EndPointParametersTask.ProductDTO
+{
+    public class ProductInputDTO
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
+        public decimal Price { get; set; }
+
+        public string Category { get; set; } = "general"; 
+    }
+}
